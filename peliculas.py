a77703553ad69b3072c5331e9b5448855a2c0215
rubenbs94 <ruben.bs.lan@gmail.com>
2024-05-29 16:22:33 +0200

    Update peliculas.py

--- peliculas.py
+++ peliculas.py
@@ -6,17 +6,20 @@ data = pd.read_csv('peliculas.csv')
 
 # Sidebar para filtros
 st.sidebar.header('Filtros')
-selected_genre = st.sidebar.multiselect('Género', options=data['Genre'].unique(), default=data['Genre'].unique())
-selected_director = st.sidebar.multiselect('Director', options=data['Director'].unique(), default=data['Director'].unique())
+selected_genre = st.sidebar.selectbox('Género', options=['All'] + list(data['Genre'].unique()))
+selected_director = st.sidebar.selectbox('Director', options=['All'] + list(data['Director'].unique()))
 selected_actor = st.sidebar.text_input('Actor')
 
 # Filtrado de datos
-filtered_data = data[data['Genre'].isin(selected_genre) & data['Director'].isin(selected_director)]
+if selected_genre != 'All':
+    data = data[data['Genre'].str.contains(selected_genre)]
+if selected_director != 'All':
+    data = data[data['Director'] == selected_director]
 if selected_actor:
-    filtered_data = filtered_data[filtered_data['Actors'].str.contains(selected_actor, case=False, na=False)]
+    data = data[data['Actors'].str.contains(selected_actor, case=False, na=False)]
 
 # Mostrar datos filtrados
-st.write(filtered_data[['Title', 'Genre', 'Director', 'Actors', 'Year', 'Revenue (Millions)', 'Rating']])
+st.write(data[['Title', 'Genre', 'Director', 'Actors', 'Year', 'Revenue (Millions)', 'Rating']])
 
 # Correr esto en la terminal:
 # streamlit run your_script_name.py
